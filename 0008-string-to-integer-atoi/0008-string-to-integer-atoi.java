583e52e1fe0750266ecc5764fa4634e5a7987cd0
Aditya Pawar <adityadpawar090@gmail.com>
2026-09-16 13:41:28 +0530

    Time: 1 ms (100%), Space: 44 MB (21.82%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,38 +1,46 @@
 class Solution {
     public int myAtoi(String s) {
-        int n = s.length();
-        int ans = 0;
-        int i = 0;
-        while (i < n && s.charAt(i) == ' ') {
-            i++;
-        }
-
+        int index = 0;
         int sign = 1;
 
-        if (i < n && s.charAt(i) == '-') {
+        while (s.length() > index && s.charAt(index) == ' ') {
+            index++;
+        }
+
+        if (s.length() > index && s.charAt(index) == '-') {
             sign = -1;
-            i++;
-        } else if (i < n && s.charAt(i) == '+') {
-            i++;
+            index++;
+        } else if (s.length() > index && s.charAt(index) == '+') {
+            index++;
         }
 
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
+        return atoi(s, index, 0, sign);
+    }
 
-            if (ans > (Integer.MAX_VALUE - digit) / 10) {
-                if (sign == 1) {
-                    return Integer.MAX_VALUE;
-                }
-                else {
-                    return Integer.MIN_VALUE;
-                }
+    public int atoi(String s, int index, long result, int sign) {
+        if (index == s.length()) {
+            return (int) (result * sign);
+        }
+
+        char ch = s.charAt(index);
+
+        if (ch < '0' || ch > '9') {
+            return (int) (result * sign);
+        }
+
+        int digit = ch - '0';
+
+        result = result * 10 + digit;
+
+        if (result > Integer.MAX_VALUE) {
+
+            if (sign == 1) {
+                return Integer.MAX_VALUE;
+            } else {
+                return Integer.MIN_VALUE;
             }
-
-            ans = ans * 10 + digit;
-            i++;
         }
-        ans = ans * sign;
 
-        return ans;
+        return atoi(s, index + 1, result, sign);
     }
 }
